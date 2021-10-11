cafa1281fbcdc0e132f4801146ceec7662676270
jeweltutin <jeweltutin@gmail.com>
2021-10-11 17:14:58 +0600

    js and image location error fixed

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,16 +5,16 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Home</title>	
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/images/favicon.ico') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets') }}/images/favicon.ico">
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,700,700italic,900,900italic&amp;subset=latin,latin-ext" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Open%20Sans:300,400,400italic,600,600italic,700,700italic&amp;subset=latin,latin-ext" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/animate.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/font-awesome.min.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/owl.carousel.min.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/chosen.min.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/color-01.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/animate.css">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/owl.carousel.min.css">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/chosen.min.css">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/style.css">
+	<link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/color-01.css">
     @livewireStyles
 </head>
 <body class="home-page home-01 ">
@@ -350,7 +350,7 @@
 								<h3 class="item-header">We Using Safe Payments:</h3>
 								<div class="item-content">
 									<div class="wrap-list-item wrap-gallery">
-										<img src="{{ asset('assets/images/payment.png" style="max-width: 260px;">
+										<img src="{{ asset('assets/images/payment.png') }}" style="max-width: 260px;">
 									</div>
 								</div>
 							</div>
@@ -379,8 +379,8 @@
 								<div class="item-content">
 									<div class="wrap-list-item apps-list">
 										<ul>
-											<li><a href="#" class="link-to-item" title="our application on apple store"><figure><img src="assets/images/brands/apple-store.png" alt="apple store" width="128" height="36"></figure></a></li>
-											<li><a href="#" class="link-to-item" title="our application on google play store"><figure><img src="assets/images/brands/google-play-store.png" alt="google play store" width="128" height="36"></figure></a></li>
+											<li><a href="#" class="link-to-item" title="our application on apple store"><figure><img src="{{ asset('assets/images/brands/apple-store.png') }}" alt="apple store" width="128" height="36"></figure></a></li>
+											<li><a href="#" class="link-to-item" title="our application on google play store"><figure><img src="{{ asset('assets/images/brands/google-play-store.png') }}" alt="google play store" width="128" height="36"></figure></a></li>
 										</ul>
 									</div>
 								</div>
@@ -463,15 +463,15 @@
 		</div>
 	</footer>
 	
-	<script src="assets/js/jquery-1.12.4.minb8ff.js?ver=1.12.4"></script>
-	<script src="assets/js/jquery-ui-1.12.4.minb8ff.js?ver=1.12.4"></script>
-	<script src="assets/js/bootstrap.min.js"></script>
-	<script src="assets/js/jquery.flexslider.js"></script>
-	<script src="assets/js/chosen.jquery.min.js"></script>
-	<script src="assets/js/owl.carousel.min.js"></script>
-	<script src="assets/js/jquery.countdown.min.js"></script>
-	<script src="assets/js/jquery.sticky.js"></script>
-	<script src="assets/js/functions.js"></script>
+	<script src="{{ asset('assets') }}/js/jquery-ui-1.12.4.minb8ff.js?ver=1.12.4"></script>
+	<script src="{{ asset('assets') }}/js/bootstrap.min.js"></script>
+	<script src="{{ asset('assets') }}/js/jquery-1.12.4.minb8ff.js?ver=1.12.4"></script>
+	<script src="{{ asset('assets') }}/js/jquery.flexslider.js"></script>
+	<script src="{{ asset('assets') }}/js/chosen.jquery.min.js"></script>
+	<script src="{{ asset('assets') }}/js/owl.carousel.min.js"></script>
+	<script src="{{ asset('assets') }}/js/jquery.countdown.min.js"></script>
+	<script src="{{ asset('assets') }}/js/jquery.sticky.js"></script>
+	<script src="{{ asset('assets') }}/js/functions.js"></script>
     @livewireScripts
 </body>
 </html>
