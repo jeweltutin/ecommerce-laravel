0e626c4346779aa91dd51edd4e27e06842466c79
jeweltutin <jeweltutin@gmail.com>
2021-10-14 17:13:56 +0600

    Product details page - Dynamic content

--- app/Http/Livewire/ProductDetailsComponent.php
+++ app/Http/Livewire/ProductDetailsComponent.php
@@ -14,6 +14,8 @@ class ProductDetailsComponent extends Component
     public function render()
     {
         $product = Product::where('slug', $this->slug)->first();
-        return view('livewire.product-details-component', ['product' => $product])->layout('layouts.base');
+        $popular_products = Product::inRandomOrder()->limit(4)->get();
+        $related_products = Product::where('category_id',$product->category_id)->inRandomOrder()->limit(8)->get();
+        return view('livewire.product-details-component', ['product' => $product, 'popular_products' => $popular_products, 'related_products' => $related_products ])->layout('layouts.base');
     }
 }
