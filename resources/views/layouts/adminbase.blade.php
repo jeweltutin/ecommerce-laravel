7a5b9644eb7a62a026c65707737cbc2b1f9f9b7a
jeweltutin <jeweltutin@gmail.com>
2021-10-25 14:09:35 +0600

    select2 implemented for select home category -backend

--- resources/views/layouts/adminbase.blade.php
+++ resources/views/layouts/adminbase.blade.php
@@ -12,6 +12,7 @@
     <meta name="description" content="This is an example dashboard created using build-in elements and components.">
     <meta name="msapplication-tap-highlight" content="no">
     <link href="{{ asset('main.css') }}" rel="stylesheet">
+    @yield('style')
     @livewireStyles
 </head>
 
--- resources/views/livewire/admin/admin-home-category-component.blade.php
+++ resources/views/livewire/admin/admin-home-category-component.blade.php
@@ -70,3 +70,17 @@
         </div>
     </div>
 </div>
+
+@section('style')
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+@endsection
+
+@section('script')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script>
+$(document).ready(function() {
+    $('.sel_categories').select2();
+});
+</script>
+@endsection
