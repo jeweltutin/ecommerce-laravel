f6ddb835569d52cc565c1530e9bc8c273bd4f113
jeweltutin <jeweltutin@gmail.com>
2021-11-02 09:55:12 +0600

    Coupons table update added needed fields with migration

---
+++ database/migrations/2021_11_02_092643_add_expirity_date_to_coupons_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class AddExpirityDateToCouponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('coupons', function (Blueprint $table) {
+            $table->date('expiry_date')->default(DB::raw('CURRENT_DATE'));
+            $table->boolean('use_status')->default(0);
+            $table->integer('usage_limit')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('coupons', function (Blueprint $table) {
+            $table->date('expiry_date')->default(DB::raw('CURRENT_DATE'));
+            $table->boolean('use_status')->default(0);
+            $table->integer('usage_limit')->nullable();
+        });
+    }
+}
