78b2acbec26348e36968f750bc2125912eff4b4f
jeweltutin <jeweltutin@gmail.com>
2022-06-06 18:09:39 +0600

    Edit product attribute - Done

--- app/Http/Livewire/Admin/AdminEditAttributeComponent.php
+++ app/Http/Livewire/Admin/AdminEditAttributeComponent.php
@@ -3,11 +3,38 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
+use App\Models\ProductAttribute;
 
 class AdminEditAttributeComponent extends Component
 {
+    public $name;
+    public $attr_id;
+
+    public function mount($attr_id){
+        $pattribute = ProductAttribute::find($attr_id);
+        $this->attr_id = $pattribute->id;
+        $this->name = $pattribute->name;
+    }
+
+    public function updated($fields){
+        $this->validateOnly($fields,[
+            "name" => "required" 
+        ]);
+    }
+
+    public function updateAttribute(){
+        $this->validate([
+            "name" => "required"
+        ]);
+
+        $pattribute = ProductAttribute::find($this->attr_id);
+        $pattribute->name = $this->name;
+        $pattribute->save();
+        session()->flash('message', 'Attribute updated seccessfully');
+    }
+
     public function render()
     {
-        return view('livewire.admin.admin-edit-attribute-component');
+        return view('livewire.admin.admin-edit-attribute-component')->layout('layouts.adminbase');
     }
 }
--- resources/views/livewire/admin/admin-edit-attribute-component.blade.php
+++ resources/views/livewire/admin/admin-edit-attribute-component.blade.php
@@ -1,3 +1,71 @@
 <div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+    <div class="app-main__inner">
+        <div class="wrap-breadcrumb">
+            <ul>
+                <li class="item-link"><a href="{{ route('admin.categories') }}" class="link">Attribute</a></li>
+                <li class="item-link"><span>Add</span></li>
+            </ul>
+        </div>
+        <div class="app-page-title">
+            <div class="page-title-wrapper">
+                <div class="page-title-heading">
+                    <div class="page-title-icon">
+                        <i class="pe-7s-car icon-gradient bg-mean-fruit">
+                        </i>
+                    </div>
+                    <div>Analytics Dashboard
+                        <div class="page-title-subheading">This is an example dashboard created using build-in elements
+                            and components.
+                        </div>
+                    </div>
+                </div>
+                <div class="page-title-actions">
+                    <button type="button" data-toggle="tooltip" title="Example Tooltip" data-placement="bottom"
+                        class="btn-shadow mr-3 btn btn-dark">
+                        <i class="fa fa-star"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="row " style="padding-bottom: 15px;">      
+            <div class="col-md-12">  
+                 <div class="main-card card">                       
+                        <div class="card-header">
+                          Edit Attribute
+                        <div class="btn-actions-pane-right">
+                            <a href="{{ route('admin.attributes') }}" class="btn btn-success pull-right">All Attributes</a>
+                        </div>                 
+                </div></div>                              
+            </div>
+        </div>
+        <div class="row justify-content-center" style="padding-top:15px;">
+            <div class="col-md-9">
+                    <div class="main-card mb-3 card">
+                        <div class="card-body">
+                            @include('includes.msgshow')
+                            <h5 class="card-title">Edit Attribute</h5>
+                            <form class="" wire:Submit.prevent="updateAttribute">
+                                <div class="position-relative form-group">
+                                    <label for="attrname" class="">Name:</label>
+                                    <input id="attrname" wire:model="name" placeholder="Attribute Name" type="text" class="form-control">
+                                    @error('name') <span class="error">{{ $message }}</span> @enderror
+                                </div>
+                                <button type="submit" class="mt-1 btn btn-primary">Update</button>
+                            </form>
+                        </div>
+                    </div>
+                <!--<div class="card widget-content bg-white">
+                    <hr />
+                </div>-->
+            </div>
+        </div>
+    </div>
 </div>
+@section('style')
+<style>
+.app-main .app-main__inner {
+    overflow: hidden;
+}
+</style>
+@endsection
+
